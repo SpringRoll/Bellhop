b1b74904154a78618535928b6ecf0ad69f96d043
Matt Karl <matt@mattkarl.com>
2014-04-13 23:21:58 -0400

    Auto updated docs

---
+++ api.js
@@ -0,0 +1,17 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Bellhop",
+        "Function.prototype.bind"
+    ],
+    "modules": [
+        "global"
+    ],
+    "allModules": [
+        {
+            "displayName": "global",
+            "name": "global"
+        }
+    ]
+} };
+});
