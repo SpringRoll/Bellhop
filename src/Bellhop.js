c255fe13790b01ec35dab89a3c26545f493df28b
Eric <ericente@cloudkid.com>
2015-02-10 14:37:18 -0500

    Added 'priority' to .on() callbacks

--- src/Bellhop.js
+++ src/Bellhop.js
@@ -180,7 +180,7 @@
 		var listeners = this._listeners[event.type];
 		if (listeners !== undefined)
 		{
-			for(var i = 0, len = listeners.length; i < len; i++)
+			for(var i = listeners.length - 1; i >= 0; i--)
 			{
 				listeners[i](event);
 			}
@@ -283,34 +283,50 @@
 	*  @param {String|Object} type The type of event to listen for or a map of events to callbacks.
 	*         Multiple events can be added by separating events with spaces.
 	*  @param {Function} callback The handler when an event is triggered
+	*  @param {int} [priority=0] The priority of the event listener. Higher numbers are handled first.
 	*  @return {Bellhop} Return instance of current object
 	*/
-	p.on = function(type, callback)
+	p.on = function(type, callback, priority)
 	{
 		if (typeof type !== "string")
 		{
 			for(var t in type)
 			{
-				this.on(t, type[t]);
+				this.on(t, type[t], priority);
 			}
 		}
 		else
 		{
 			var types = type.split(" ");
-
+			var listener;
 			for(var i = 0, len = types.length; i < len; i++)
 			{
 				type = types[i];
 
-				if (this._listeners[type] === undefined)
+				listener = this._listeners[types];
+				if(!listener)
+					listener = this._listeners[types] = [];
+
+				callback._priority = parseInt(priority) || 0;
+
+				if (listener.indexOf(callback) === -1)
 				{
-					this._listeners[type] = [];
+					listener.push(callback);
+					if(listener.length > 1)
+						listener.sort(listenerSorter);
 				}
-				this._listeners[type].push(callback);
 			}
 		}
 		return this;
 	};
+
+	/**
+	*  Sorts listeners added by .on() by priority
+	*/
+	function listenerSorter(a, b)
+	{
+		return a._priority - b._priority;
+	}
 
 	/**
 	*  Remove an event listener
