e59ac8a0ff16ab3667420e875ff713215739a968
Chip Bell <cbell@pbs.org>
2018-05-09 14:56:31 -0400

    Removing library-grunt as a dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,29 @@
 module.exports = function(grunt)
 {
-	require('library-grunt')(grunt,
-	{
-		themePath: '../SpringRollTheme'
-	});
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    eslint: {
+      bellhop: ['src/*']
+    },
+    concat: {
+      options: {
+        banner: '/* <%= pkg.name %> <%= pkg.version %> */\n'
+      },
+      bellhop: {
+        src: ['src/BellhopEventDispatcher.js', 'src/Bellhop.js'],
+        dest: 'dist/bellhop.js'
+      }
+    },
+    uglify: {
+      bellhop: {
+        files: {
+          'dist/bellhop.min.js' : ['dist/bellhop.js']
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['eslint', 'concat', 'uglify']);
 };
