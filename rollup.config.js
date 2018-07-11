5fdc8a34e798f9673fd1db0fc4b546db557995ed
Andrew Smith <andrew.smith@redspace.com>
2018-07-11 14:55:58 -0300

    Added multiple formats to allow for different environments

--- rollup.config.js
+++ rollup.config.js
@@ -3,19 +3,29 @@ import commonjs from 'rollup-plugin-commonjs';
 import eslint from 'rollup-plugin-eslint';
 import prettier from 'rollup-plugin-prettier';
 import uglify from 'rollup-plugin-uglify';
-import pkg from './package.json';
 import babel from 'rollup-plugin-babel';
 
 export default [
   {
     input: 'src/Bellhop.js',
-    external: ['ms'],
     output: [
       {
-        file: pkg.main,
+        file: 'bellhop.js',
+        format: 'es',
+        sourceMap: true,
+        extend: true
+      },
+      {
+        file: 'ellhop.umd.js',
         format: 'umd',
         sourceMap: true,
         name: 'window',
+        extend: true
+      },
+      {
+        file: 'bellhop.cjs.js',
+        format: 'cjs',
+        sourceMap: true,
         extend: true
       }
     ],
