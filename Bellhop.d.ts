0113f623ddf2e70762f2d0f73f6539fcb168500d
Andrew Smith <andrew.smith@redspace.com>
2018-08-13 08:42:26 -0300

    removed scoping from typescript definition file

--- Bellhop.d.ts
+++ Bellhop.d.ts
@@ -1,35 +1,35 @@
 export as namespace BellhopIframe;
 
 interface PriorityFunction extends Function {
-  private _priority: number;
+  _priority: number;
 }
 
 export class BellhopEventDispatcher {
   constructor();
-  on(name: string, callback: Function, priority?: number): void;
-  off(name: string, callback: Function): void;
-  trigger(event: object | string): void;
   destroy(): void;
-  private listenerSorter(a: PriorityFunction, b: PriorityFunction): number;
+  listenerSorter(a: PriorityFunction, b: PriorityFunction): number;
+  off(name: string, callback: Function): void;
+  on(name: string, callback: Function, priority?: number): void;
+  trigger(event: object | string): void;
 
 }
 
 export class Bellhop extends BellhopEventDispatcher {
   constructor(id?: number | string);
-  private id: string;
-  private connected: boolean;
-  private isChild: boolean;
-  private connecting: boolean;
-  private origin: string;
-  private _sendLater: any[];
-  private iframe: HTMLIFrameElement | null;
-  private receive(message: MessageEvent);
-  private onConnectionReceived(message: object);
+  _sendLater: any[];
+  connected: boolean;
+  connecting: boolean;
+  id: string;
+  iframe: HTMLIFrameElement | null;
+  isChild: boolean;
+  origin: string;
   connect(iframe?: HTMLIFrameElement, origin?: string ): Bellhop;
-  disconnect(): void;
-  send(type: string, data?: any): void;
-  fetch(event: string, callback: Function, data?: object, runOnce?: boolean): void;
-  respond(event: string, data?: object, runOnce?: boolean): void;
   destroy(): void;
+  disconnect(): void;
+  fetch(event: string, callback: Function, data?: object, runOnce?: boolean): void;
+  onConnectionReceived(message: object);
+  receive(message: MessageEvent);
+  respond(event: string, data?: object, runOnce?: boolean): void;
+  send(type: string, data?: any): void;
   target(): Window | HTMLIFrameElement | ParentNode;
 }
